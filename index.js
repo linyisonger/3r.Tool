665bf011b62b325ee1796a8d4500b945c7df047d
linyisonger <752957127@qq.com>
2023-10-12 19:26:55 +0800

    调整一下执行顺序

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-export * from './lib/common.js';
-export * from './lib/vertor2.js';
-export * from './lib/maths.js';
-export * from './lib/map.js';
-export * from './lib/randoms.js';
-export * from './lib/convertor.js';
-export * from './lib/verify.js';
-export * from './lib/market.js';
-export * from './lib/animation.js';
-export * from './lib/picture.js';
-export * from './lib/generate.js';
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./lib/common.js"), exports);
+__exportStar(require("./lib/vertor2.js"), exports);
+__exportStar(require("./lib/maths.js"), exports);
+__exportStar(require("./lib/map.js"), exports);
+__exportStar(require("./lib/randoms.js"), exports);
+__exportStar(require("./lib/convertor.js"), exports);
+__exportStar(require("./lib/verify.js"), exports);
+__exportStar(require("./lib/market.js"), exports);
+__exportStar(require("./lib/animation.js"), exports);
+__exportStar(require("./lib/picture.js"), exports);
+__exportStar(require("./lib/generate.js"), exports);
